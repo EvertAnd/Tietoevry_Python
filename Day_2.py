a15e8923de59219473572e30e379a9be46870e2c
EvertAnd <100149570+EvertAnd@users.noreply.github.com>
2022-02-23 17:28:14 +0200

    Merge remote-tracking branch 'github/main'

--- Day_2.py
+++ Day_2.py
@@ -1,25 +1,15 @@
 # Day 2 - Work in Class
 # 1. Exercise - Age 100
-#
+
 # Write a program that asks for and saves a username                                         ok
 # Ask a question about the user's age, using the username in the question.                    ok
 # Shows in how many years the user will be 100 years old smile                                ok
-# BONUS: Let the program also show the year when the user will be 100 years old.
+# BONUS: Let the program also show the year when the user will be 100 years old.              ok
 # It could use a variable with the current year.
 # It would be even better to get the current year automatically
 # then you will need two additional lines:
 # import datetime # let's talk about imports separately
 # currentYear = datetime.datetime.now (). year
-# 2. Exercise - Room volume
-#
-# Ask user to input 3 numbers - width, length, height
-# Find the volume of the room
-# PS Think about units and what is the most appropriate data type for this
-# 3. Exercise - Temperature Conversion
-#
-# Write a program that asks user for temperature in Celsius and prints out this same temperature in Farenheit
-# formula is: farenheit = 32+celsius*(9/5)
-# PS Remember about data type conversion, also consider precision
 
 #_______________________________________1.uzdevums____________________________________
 import datetime
@@ -35,3 +25,21 @@ print(f"Jauki, tad tu pēc {till_old} būsi jau 100 gadus vecs :)")
 print(f"Un {old_age_after}. gadā tu būsi tieši 100 gadus vecs!")
 
 #_______________________________________2.uzdevums____________________________________
+# 2. Exercise - Room volume
+#
+# Ask user to input 3 numbers - width, length, height
+# Find the volume of the room
+# PS Think about units and what is the most appropriate data type for this
+width = float(input("Ievadi istabas platumu"))
+length = float(input("Ievadi istabas garumu"))
+height = float(input("Ievadi istabas augstumu"))
+room_vol = width * length * height
+print(f"Istabas kubitūra ir {room_vol} m\u00b3")
+
+#_______________________________________3.uzdevums____________________________________
+
+# 3. Exercise - Temperature Conversion
+#
+# Write a program that asks user for temperature in Celsius and prints out this same temperature in Farenheit
+# formula is: farenheit = 32+celsius*(9/5)
+# PS Remember about data type conversion, also consider precision
