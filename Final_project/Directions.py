a94b7ad64289c7fdf4a47e1013f61ac949ca49fb
EvertAnd <100149570+EvertAnd@users.noreply.github.com>
2022-04-18 12:22:04 +0300

    Lets Start by Finding Location Rīga -> Liepāja

--- Final_project/Directions.py
+++ Final_project/Directions.py
@@ -1,3 +1,19 @@
-# Hello
+import time
+from typing import List
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
-# Final Puš
+driver = webdriver.Chrome("C:\Windows\Temp\chromedriver.exe")  # set path to chromedriver if you have no PATH
+driver.set_window_size(1024, 600)
+driver.maximize_window()
+url = "https://www.bing.com/maps/directions"
+driver.get(url)  # this will open the url in browser
+# time.sleep(2)  # this will wait for 2 seconds
+element = driver.find_elements(By.CLASS_NAME, "wayPointsContainer")
+element = driver.find_elements(By.CLASS_NAME, "dirBtnGo commonButton")
+
+driver.find_element(By.XPATH, '//*[@placeholder="From"]').send_keys("Riga")
+driver.find_element(By.XPATH, '//*[@placeholder="To"]').send_keys("Liepaja")
+pyautogui.press('enter')
