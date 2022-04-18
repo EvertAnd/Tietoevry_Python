1e53872b68e0e38becbaaaf89ed12d298953ee02
EvertAnd <100149570+EvertAnd@users.noreply.github.com>
2022-04-18 23:13:07 +0300

    Lets Start by Finding Location Rīga -> Liepāja Input Method set. direction_from = input("Ievadi sākuma adresi: ") direction_to = input("Ievadi galamērķi: ")

--- Final_project/Directions.py
+++ Final_project/Directions.py
@@ -1,19 +1,28 @@
 import time
 from typing import List
+import pandas as pd
 import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+direction_from = input("Ievadi sākuma adresi: ")
+direction_to = input("Ievadi galamērķi: ")
 
 driver = webdriver.Chrome("C:\Windows\Temp\chromedriver.exe")  # set path to chromedriver if you have no PATH
 driver.set_window_size(1024, 600)
 driver.maximize_window()
 url = "https://www.bing.com/maps/directions"
-driver.get(url)  # this will open the url in browser
+driver.get(url)
 # time.sleep(2)  # this will wait for 2 seconds
+
 element = driver.find_elements(By.CLASS_NAME, "wayPointsContainer")
 element = driver.find_elements(By.CLASS_NAME, "dirBtnGo commonButton")
 
-driver.find_element(By.XPATH, '//*[@placeholder="From"]').send_keys("Riga")
-driver.find_element(By.XPATH, '//*[@placeholder="To"]').send_keys("Liepaja")
+driver.find_element(By.XPATH, '//*[@placeholder="From"]').send_keys(direction_from)
+driver.find_element(By.XPATH, '//*[@placeholder="To"]').send_keys(direction_to)
 pyautogui.press('enter')
+time.sleep(2)  # this will wait for 2 seconds
+pyautogui.press('Tab')
+pyautogui.press('Enter')
+pyautogui.press('Tab')
+pyautogui.press('Enter')
